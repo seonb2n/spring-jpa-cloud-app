a091aa5378767d1d0d08b16ad8965b3718f893d8
seonb2n <626ksb@gmail.com>
2022-11-17 21:59:58 +0900

    - User 조회시 N+1 문제를 해결할 수 있도록 쿼리 튜닝 테스트 구현

--- src/main/java/com/example/demo/infrastructure/project/ProjectRepository.java
+++ src/main/java/com/example/demo/infrastructure/project/ProjectRepository.java
@@ -1,7 +1,12 @@
 package com.example.demo.infrastructure.project;
 
 import com.example.demo.domain.project.Project;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +15,8 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     Optional<Project> findProjectByProjectToken(String projectToken);
 
     List<Project> findAllByUserToken(String userToken);
+
+    @EntityGraph(attributePaths = {"user"}, type = EntityGraph.EntityGraphType.FETCH)
+    @Query("select p from Project p left join p.user where p.user.id = :userId")
+    Page<Project> findAllPage(Pageable pageable, @Param("userId") Long userId);
 }
