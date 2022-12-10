54167aae2f809b720fba1706c5517342fa1785d4
seonb2n <626ksb@gmail.com>
2022-12-10 16:29:16 +0900

    - service 메서드 주석 추가

--- src/main/java/com/example/demo/domain/project/service/ProjectService.java
+++ src/main/java/com/example/demo/domain/project/service/ProjectService.java
@@ -6,16 +6,51 @@ import com.example.demo.domain.user.User;
 
 public interface ProjectService {
 
+    /**
+     * 새로운 project 를 등록하는 메서드
+     * @param registerProject
+     * @return 프로젝트 고유 token
+     */
     String registerProject(ProjectCommand.RegisterProject registerProject);
 
+    /**
+     * 초기 회원 가입 시 태스크 등록을 받을 수 있도록 빈 프로젝트를 생성하는 메서드
+     * @param userToken
+     * @return 프로젝트 고유 token
+     */
     String registerNoneProject(String userToken);
 
+    /**
+     * 프로젝트의 내용을 업데이트하는 메서드
+     * @param updateProject
+     * @return 프로젝트 고유 token
+     */
     String updateProject(ProjectCommand.UpdateProject updateProject);
 
+    /**
+     * task 를 등록하는 메서드
+     * @param registerTask
+     * @return 태스크 고유 token
+     */
     String registerTask(ProjectCommand.RegisterTask registerTask);
 
+    /**
+     * task 를 업데이트하는 메서드
+     * @param updateTask
+     * @return 태스크 고유 token
+     */
     String updateTask(ProjectCommand.UpdateTask updateTask);
 
+    /**
+     * task 하위의 action 을 등록하는 메서드
+     * @param registerAction
+     * @return 액션 고유 token
+     */
     String registerAction(ProjectCommand.RegisterAction registerAction);
 
+    /**
+     * 프로젝트 고유 token 으로 프로젝트를 가져오는 메서드
+     * @param projectToken
+     * @return ProjectInfo
+     */
     ProjectInfo.Main retrieveProject(String projectToken);}
--- src/main/java/com/example/demo/domain/user/service/UserService.java
+++ src/main/java/com/example/demo/domain/user/service/UserService.java
@@ -4,9 +4,26 @@ import com.example.demo.domain.user.UserCommand;
 import com.example.demo.domain.user.UserInfo;
 
 public interface UserService {
+
+    /**
+     * 사용자 등록하는 메서드
+     * @param registerUserCommand
+     * @return UserInfo.Main
+     */
     UserInfo.Main registerUser(UserCommand.RegisterUser registerUserCommand);
 
+    /**
+     * 사용자 고유 token 을 바탕으로 사용자 정보를 가져오는 메서드
+     * @param userToken
+     * @return UserInfo.Main
+     */
     UserInfo.Main getUser(String userToken);
 
+    /**
+     * 사용자 로그인하는 메서드
+     * @param userEmail
+     * @param userPassword
+     * @return
+     */
     UserInfo.Main loginUser(String userEmail, String userPassword);
 }
